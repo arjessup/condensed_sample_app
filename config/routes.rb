a7eb0c0d5c2c26ccc4f3638f2dd25891cac89f7c
Andrea Jessup <jessup.andrea@gmail.com>
2012-04-08 18:14:30 -0500

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 CondensedSampleApp::Application.routes.draw do
 
+  resources :sessions, :only => [:new, :create, :destroy]
+
   resources :users
 
-  match '/signup', :to => 'users#new'
-  match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
+  match '/signup',    :to => 'users#new'
+  match '/signin',    :to => 'sessions#new'
+  match '/signout',   :to => 'sessions#destroy'
+  match '/contact',   :to => 'pages#contact'
+  match '/about',     :to => 'pages#about'
+  match '/help',      :to => 'pages#help'
 
   root :to => 'pages#home'
 
